7117500b8f0c7bebbbb956a196f5b3e8365e19a3
weyob <weyob@uw.edu>
2021-03-13 12:08:45 -0800

    Lab 2: step 3.3
    added a "Done" button and matching IBAction "didTapOnDone"

--- Flashcards/CreationViewController.swift
+++ Flashcards/CreationViewController.swift
@@ -19,14 +19,7 @@ class CreationViewController: UIViewController {
         dismiss(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapOnDone(_ sender: Any) {
     }
-    */
-
+    
 }
